eef0ea3206043b0846734eb165826b1a770d6935
Sergio Andrés Delgado Quevedo <s.delgadoq@uniandes.edu.co>
2025-09-04 20:54:42 -0500

    Part 2: Model estimation and FWL

--- scripts/GenderEarningsGap.r
+++ scripts/GenderEarningsGap.r
@@ -1,13 +1,47 @@
 # -----------------------------------------------------
-# PART 3: Gender Earnings Gap
+# PART 2: Gender Earnings Gap
 # This script is for downloading data from the workshop.
-# 1) d
-# 2) d
-# 3) d
-# 4) d
+# 1) Estimating the unconditional wage gap
+# 2) Conditional earnings gap with worker and job controls
+#   2.1) with FWL
+#   2.2) with FWL and bootstrap
+# 3) Plot predicted age-wage profile with confidence intervals by gender.
+#   3.1) Estimate age-wage profile
+#   3.2) Estimate peak ages
+#   3.3) Estimate confidence intervals
 # -----------------------------------------------------
 
+# -----------------------------------------------------
+# 1) Estimating the unconditional wage gap
+# -----------------------------------------------------
+
+# Model estimation
+wage_gap_model <- lm(log(salario) ~ sexo, data = db)
+summary(wage_gap_model)
 
 # -----------------------------------------------------
-# 1) d
+# 2.1) Conditional earnings gap with worker and job controls (FWL)
 # -----------------------------------------------------
+
+# Define variables for control and fixed effects
+#TODO: cambiar controles y FE
+controls <- c("edad", "edad2", "educ", "educ2", "experiencia", "experiencia2")
+fixed_effects <- c("ocupacion", "industria", "region")
+
+# FWL approach
+# Step 1: Partial out controls from log(salario)
+residuals_y <- lm(log(salario) ~ ., data = db[, c("log(salario)", controls)])$residuals
+
+# Step 2: Partial out controls from sexo
+residuals_x <- lm(sexo ~ ., data = db[, c("sexo", controls)])$residuals
+
+# Step 3: Regress residuals_y on residuals_x
+fwl_model <- lm(residuals_y ~ residuals_x)
+summary(fwl_model)
+
+
+# Fixed effects 
+# TODO: ver cómo meter estos FE en el modelo de FWL
+controls <- c("edad", "edad2", "educ", "educ2", "experiencia", "experiencia2", "ocupacion", "industria", "region")
+formula <- as.formula(paste("log(salario) ~ sexo |", paste(controls, collapse = " + ")))
+conditional_wage_gap_model <- felm(formula, data = db)
